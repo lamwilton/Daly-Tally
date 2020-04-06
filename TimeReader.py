daf60ce1c21526b909215def32536c25235275b3
lamwilton <lamwiltonusa@gmail.com>
2020-04-06 01:59:42 -0700

    US

--- TimeReader.py
+++ TimeReader.py
@@ -9,6 +9,7 @@ import datetime
 class TimeReader:
     def __init__(self):
         self.df = pd.read_csv("TimeSeries.csv")
+        self.sumdf = pd.DataFrame()
         self.cleaningData()
 
     def cleaningData(self):
@@ -24,7 +25,12 @@ class TimeReader:
         day0 = datetime.date(2020, 1, 15)
         self.df['dateChecked'] = pd.to_datetime(self.df['dateChecked']).dt.date
         self.df['day'] = (self.df['dateChecked'] - day0).dt.days
-        print()
+
+        # Compute sum of all states for USA
+        df = self.df
+        self.sumdf = ps.sqldf("SELECT dateChecked, SUM(death) FROM df GROUP BY dateChecked")
+        self.sumdf['dateChecked'] = pd.to_datetime(self.sumdf['dateChecked']).dt.date
+        self.sumdf['day'] = (self.sumdf['dateChecked'] - day0).dt.days
 
     def getStateData(self, state):
         """
@@ -36,6 +42,9 @@ class TimeReader:
         df = self.df
         stateData = ps.sqldf("SELECT * from df WHERE state = '" + state + "'")
         return stateData
+
+    def getsumDf(self):
+        return self.sumdf
 
 
 class SigmoidFitter:
@@ -72,8 +81,8 @@ class SigmoidFitter:
         temp = np.arange(0, np.size(self.xdata))
         sigma = 1 / temp ** 2
 
-        p0 = [1, 80, 2000]  # this is an mandatory initial guess
-        self.popt, self.pcov = curve_fit(self.sigmoid, self.xdata, self.ydata, p0=p0, sigma=sigma)
+        p0 = [1, 100, 10000]  # this is an mandatory initial guess
+        self.popt, self.pcov = curve_fit(self.sigmoid, self.xdata, self.ydata, p0=p0, sigma=sigma, maxfev=1000000)
 
     def compute(self):
         """
@@ -82,6 +91,7 @@ class SigmoidFitter:
         """
         self.fit()
         perr = np.sqrt(np.diag(self.pcov))
+        print("State: " + str(self.state))
         print("Speed: " + str(self.popt[0]) + " +/- " + str(perr[0]))
         print("Day of the half point: " + str(self.popt[1]) + " +/- " + str(perr[1]))
         print("Total number: " + str(self.popt[2]) + " +/- " + str(perr[2]))
@@ -99,22 +109,40 @@ class SigmoidFitter:
         x = np.linspace(2, 500, 5000)
         y = self.sigmoid(x, *self.popt)
         plt.plot(x, y)
-        # plt.yscale('log')
+
         plt.xlim(45, 120)
         plt.ylim(0, max(y) * 1.1)
         plt.savefig(self.state + "_sigmoid.png", transperent=True)
+
+        plt.yscale('log')
+        plt.ylim(1, max(y) * 1.1)
+        plt.savefig(self.state + "_sigmoid_log.png", transperent=True)
         plt.show()
 
 
 def main():
-    state = "CA"
+    states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN",
+              "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH",
+              "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX",
+              "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    states = []
     timereader = TimeReader()
-    stateData = timereader.getStateData(state)
-    death = stateData['death']
-    day = stateData['day']
+    for state in states:
+        stateData = timereader.getStateData(state)
+        death = stateData['death']
+        day = stateData['day']
 
+        # Curve Fitting
+        sigmoidfitter = SigmoidFitter(xdata=day, ydata=death, state=state)
+        sigmoidfitter.compute()
+        sigmoidfitter.plot()
+
+    # National data
+    sumdf = timereader.getsumDf()
+    death = sumdf['SUM(death)']
+    day = sumdf['day']
     # Curve Fitting
-    sigmoidfitter = SigmoidFitter(xdata=day, ydata=death, state=state)
+    sigmoidfitter = SigmoidFitter(xdata=day, ydata=death, state="US")
     sigmoidfitter.compute()
     sigmoidfitter.plot()
     exit()
